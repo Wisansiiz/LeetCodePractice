8d3fd879bbcd58b1542cfbb22028478c2761bffc
Wisansiiz <18257719723@163.com>
2022-09-23 22:51:04 +0800

    每日一题

--- src/myLinkedList_707/MyLinkedList.java
+++ src/myLinkedList_707/MyLinkedList.java
@@ -19,33 +19,46 @@ class MyLinkedList {
     }
 
     public int get(int index) {
-
-        return 0;
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        Node cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
     }
 
     public void addAtHead(int val) {
-
+        addAtIndex(0, val);
     }
 
     public void addAtTail(int val) {
-
+        addAtIndex(size, val);
     }
 
     public void addAtIndex(int index, int val) {
-
+        if (index > size)
+            return;
+        index = Math.max(0, index);
+        size++;
+        Node pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        Node add = new Node(val);
+        add.next = pred.next;
+        pred.next = add;
     }
 
     public void deleteAtIndex(int index) {
-
+        if (index >= size || index < 0)
+            return;
+        size--;
+        Node pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        pred.next = pred.next.next;
     }
 }
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
